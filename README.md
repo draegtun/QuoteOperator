7b08bd31f1632992c75627027488404a86ef8de1
draegtun <draegtun@googlemail.com>
2013-03-29 14:38:42 +0000

    started project

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+QuoteOperator - Blah blah blah
+
+# SYNOPSIS
+
+    use QuoteOperator;
+
+# DESCRIPTION
+
+QuoteOperator is
+
+# AUTHOR
+
+Barry Walsh <draegtun@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2013- Barry Walsh
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/QuoteOperator.pm
@@ -0,0 +1,39 @@
+package QuoteOperator;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+QuoteOperator - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use QuoteOperator;
+
+=head1 DESCRIPTION
+
+QuoteOperator is
+
+=head1 AUTHOR
+
+Barry Walsh E<lt>draegtun@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Barry Walsh
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
